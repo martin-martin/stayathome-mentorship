a46840dc4e1d6b7a68ad4e1d65755c563e00fc43
martin-martin <breuss.martin@gmail.com>
2020-03-24 15:31:18 +0200

    Update DEBUG settings

--- mentor_match/settings.py
+++ mentor_match/settings.py
@@ -25,7 +25,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'devlopment')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+try:
+    if os.environ['DEBUG'] == 'True':
+        DEBUG = True
+    else:
+        DEBug = False
+except KeyError:
+    DEBUG = False
 
 ALLOWED_HOSTS = []
 
