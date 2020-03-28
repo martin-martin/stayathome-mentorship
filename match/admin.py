bed420206ecdc5911cc62c3c151cb1b13755f2ca
martin-martin <breuss.martin@gmail.com>
2020-03-28 18:02:46 +0200

    Add CSV export functionality

--- match/admin.py
+++ match/admin.py
@@ -1,6 +1,27 @@
+import csv
+from django.http import HttpResponse
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
 from .models import Student, Mentor, Skill #, Person
+
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected to CSV"
 
 
 class StudentInLine(admin.TabularInline):
@@ -16,7 +37,7 @@ class StudentInLine(admin.TabularInline):
 #     extra = 0
 
 
-class StudentAdmin(admin.ModelAdmin):
+class StudentAdmin(admin.ModelAdmin, ExportCsvMixin):
     list_display = ('name', 'email', 'timezone', 'has_mentor', 'is_active')
     list_filter = ['timezone', 'lost_job', 'timestamp', 'skills']
     search_fields = ['name', 'email', 'skills']
@@ -26,8 +47,10 @@ class StudentAdmin(admin.ModelAdmin):
         ('Time Info', {'fields': ['timezone', 'daytime', 'start_date', 'end_date']}),
     ]  # omitting field 'timestamp' (for when the form was submitted) and 'lost_job'
 
+    actions = ["export_as_csv"]
 
-class MentorAdmin(admin.ModelAdmin):
+
+class MentorAdmin(admin.ModelAdmin, ExportCsvMixin):
     list_display = ('name', 'email', 'timezone', 'weeks', 'capacity', 'has_capacity')
     list_filter = ['timezone', 'weeks', 'timestamp', 'skills']
     search_fields = ['name', 'email', 'skills']
@@ -40,6 +63,8 @@ class MentorAdmin(admin.ModelAdmin):
         ('Skills', {'fields': ['skills', 'details']}),
         ('Time Info', {'fields': ['timezone', 'daytime']}),
     ]  # omitting field 'timestamp' (for when the form was submitted)
+
+    actions = ["export_as_csv"]
 
 
 class SkillAdmin(admin.ModelAdmin):
